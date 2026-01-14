5d8108ced7611092ecec5e4d34e5dbb36034f08f
weinbenni <weinhaeuplb@outlook.de>
2026-01-14 09:45:38 +0100

    Redid project structure

--- js/main.js
+++ js/main.js
@@ -1,11 +1,7 @@
 // Main application logic for Excel to Cards Importer
 class ExcelToCardsImporter {
   constructor() {
-    this.t = window.TrelloPowerUp.iframe({
-      appKey: 'c9df6f6f1cd31f277375aa5dd43041c8',
-      appName: 'Excel to Cards Importer',
-      appAuthor: 'Excel PowerUp Developer'
-    });
+    this.t = window.TrelloPowerUp.iframe();
     
     this.excelData = null;
     this.columns = [];
