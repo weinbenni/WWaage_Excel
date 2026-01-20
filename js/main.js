85223104885eafaf51d9e31d31e4c38e163bca24
weinbenni <weinhaeuplb@outlook.de>
2026-01-20 14:33:16 +0100

    Debug on parseSyntax

--- js/main.js
+++ js/main.js
@@ -794,7 +794,8 @@ class ExcelToCardsImporter {
 
       // Evaluate the expression
       // eslint-disable-next-line no-eval
-      const result = eval(expression);
+      const result = eval(expression);#
+      console.log('Evaluated syntax result:', result);
       return String(result || '');
     } catch (error) {
       console.error('Syntax parsing error:', error, 'Syntax:', syntax);
